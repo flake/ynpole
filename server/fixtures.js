f76457f3b4d26b059aca8f3567e46ec5aed33ceb
Rajasekhar <raj.cynosure@gmail.com>
2015-05-17 00:56:06 +0530

    font awesome icons rendered

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,82 @@
 if (Posts.find().count() == 0){
+	var now = new Date().getTime();
+
+	var samId = Meteor.users.insert({
+		profile: {name: 'Uncle Sam'}
+	});
+
+	var ramId = Meteor.users.insert({
+		profile: {name: 'Rambo Ramesh'}
+	});
+
+	var sam = Meteor.users.findOne(samId);
+	var ram = Meteor.users.findOne(ramId);
+
+	var questId = Posts.insert({
+		question: 'Is voting review assertive?',
+		topic: 'public',
+		source_id: sam._id,
+		asked_by: sam.profile.name,
+		polarity: 0,
+		yes_voters: [],
+		no_voters: [],
+		yes_votes: 0,
+		no_votes: 0,
+		created_at: now - 3 * 3600 * 1000
+	});
+/*
+	Comments.insert({
+		postId: questId,
+		userId: ram._id,
+		author: ram.profile.name,
+		comment: 'this clears the confusion and makes assertive',
+		created_at: now - 2 * 3600 * 1000
+	});
+
+	Comments.insert({
+		postId: questId,
+		userId: sam._id,
+		author: sam.profile.name,
+		comment: 'helps to make healthy decisions',
+		created_at: now - 1 * 3600 * 1000
+	});
+*/
 	Posts.insert({
 		question: 'Does it look good?',
-		topic: 'design'
+		topic: 'design',
+		source_id: ram._id,
+		asked_by: ram.profile.name,
+		polarity: 0,
+		yes_voters: [],
+		no_voters: [],
+		yes_votes: 0,
+		no_votes: 0,
+		created_at: now - 5 * 3600 * 1000
 	});
 
 	Posts.insert({
 		question: 'Is it helpful?',
-		topic: 'service'
+		topic: 'service',
+		source_id: sam._id,
+		asked_by: sam.profile.name,
+		polarity: 0,
+		yes_voters: [],
+		no_voters: [],
+		yes_votes: 0,
+		no_votes: 0,
+		created_at: now - 6 * 3600 * 1000
 	});
 
 	Posts.insert({
 		question: 'I believe this is gonna rock, what say you?',
-		topic: 'opinion'
+		topic: 'opinion',
+		source_id: ram._id,
+		asked_by: ram.profile.name,
+		polarity: 0,
+		yes_voters: [],
+		no_voters: [],
+		yes_votes: 0,
+		no_votes: 0,
+		created_at: now - 7 * 3600 * 1000
 	});
 }
