c2b7af69bd2beff5502d17e49912aab5d905e2f7
Rajasekhar <raj.cynosure@gmail.com>
2015-05-16 16:25:07 +0530

    profile view activity posts

--- server/publications.js
+++ server/publications.js
@@ -5,3 +5,18 @@ Meteor.publish('posts', function(){
 Meteor.publish('userInfo', function(userId){
 	return Meteor.users.find(userId);
 });
+
+/*
+Meteor.publish('userInfo', function(userId){
+	Meteor.publishWithRelations({
+		handle: this,
+		collection: users,
+		filter: _id,
+		mappings: [
+			{
+				foreign_key: "source_id",
+				collection: "posts"
+			}
+		]
+	})
+}); */
