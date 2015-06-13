2b79a0e34eaec180a9806817147aa7b166cfdc39
Rajasekhar <raj.cynosure@gmail.com>
2015-06-14 03:13:18 +0530

    login validation errors with css arrow box

--- client/views/application/login.js
+++ client/views/application/login.js
@@ -1,14 +1,76 @@
 Template.login.events({
 	'submit form':function(event, template){
 		event.preventDefault();
-		var emailVar = template.find('#login-email').value;
-		var passwordVar = template.find('#login-password').value;
-		Meteor.loginWithPassword(emailVar, passwordVar);
+		if(!Session.get('email-err')){
+			var emailVar = template.find('#login-email').value;
+			var passwordVar = template.find('#login-password').value;
+			Meteor.loginWithPassword(emailVar, passwordVar, function(error){
+				if(error){
+					if(error.reason === "User not found"){
+						Session.set('email-err', error.reason);
+						$('#login-email').css("border", "1px solid #CF4E4E");
+						$('#login-email').siblings(".errspan").css("visibility", "visible");
+					}
+					if(error.reason === "Incorrect password"){
+						Session.set('password-err', error.reason);
+						$('#login-password').css("border", "1px solid #CF4E4E");
+						$('#login-password').siblings(".errspan").css("visibility", "visible");
+					}
+				}
+			});
+		}
 	},
 
 	'click #remember-text': function(event, template){
 		var remember = $('#remember');
 		remember.prop("checked", !remember.prop("checked"));
+	},
+
+	'blur #login-email': function(event, template){
+		event.preventDefault();
+		Session.set('email-err', false);
+		$('.err-arrow-box').css({"visibility": "hidden"});
+
+		var valid = validateEmail($(event.currentTarget).val());
+		if(!valid){
+			Session.set('email-err', "Not a valid email address.");
+			$(event.currentTarget).css("border", "1px solid #CF4E4E");
+			$(event.currentTarget).siblings(".errspan").css("visibility", "visible");
+		}
+	},
+
+	'focus #login-email': function(event, template){
+		event.preventDefault();
+		if(Session.get('email-err')){
+			var gridPos = $('.login-grid').offset();
+			var fieldPos = $(event.currentTarget).position();
+			var top = gridPos.top + fieldPos.top;
+			var right = gridPos.left -460;
+			$('.err-msg').text(Session.get('email-err'));
+			$('.err-arrow-box').css({'top':top, 'right':right, "visibility": "visible"});
+			$(event.currentTarget).css("border", "1px solid #D0D1D5");
+			$(event.currentTarget).siblings(".errspan").css("visibility", "hidden");
+		}
+	},
+
+	'blur #login-password': function(event, template){
+		event.preventDefault();
+		Session.set('password-err', false);
+		$('.err-arrow-box').css({"visibility": "hidden"});
+	},
+
+	'focus #login-password': function(event, template){
+		event.preventDefault();
+		if(Session.get('password-err')){
+			var gridPos = $('.login-grid').offset();
+			var fieldPos = $(event.currentTarget).position();
+			var top = gridPos.top + fieldPos.top;
+			var right = gridPos.left -460;
+			$('.err-msg').text(Session.get('password-err'));
+			$('.err-arrow-box').css({'top':top, 'right':right, "visibility": "visible"});
+			$(event.currentTarget).css("border", "1px solid #D0D1D5");
+			$(event.currentTarget).siblings(".errspan").css("visibility", "hidden");
+		}
 	}
 });
 
@@ -19,3 +81,8 @@ Template.layout.events({
 		Router.go('/');
 	}
 });
+
+Template.layout.created = function(){
+	Session.set('email-err', false);
+	Session.set('password', false);
+}
