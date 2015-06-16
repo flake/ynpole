714f20c35c9f1534401019b5dcfe591e26fb4024
Rajasekhar <raj.cynosure@gmail.com>
2015-06-17 01:54:52 +0530

    smtp postfix from aws

--- server/smtp.js
+++ server/smtp.js
@@ -1,10 +1,13 @@
 Meteor.startup(function(){
-	smtp = {
+/*	smtp = {
 		username: 'AKIAJVLPILSNP3RIZS5A',
 		password: 'AjYkxdOoibBSZthk1d3PXSoSu9hNrjqQ0dam/5HvdEUn',
 		server: 'email-smtp.us-west-2.amazonaws.com',
+		server: localhost
 		port: 25
 	}
 
 	process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+*/
+	process.env.MAIL_URL = 'smtp://localhost:25';
 });
