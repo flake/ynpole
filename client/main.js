77308ac8f1673d01896f361d59a2b8d7c7ebfadd
Rajasekhar <raj.cynosure@gmail.com>
2015-06-17 19:49:21 +0530

    userId for email verification

--- client/main.js
+++ client/main.js
@@ -20,10 +20,13 @@ Accounts.onEmailVerificationLink(function(token, done){
 		Accounts.verifyEmail(token, function(err){
 			if(err != null){
 				if(err.message == 'Verify email link expired [403]'){
-				/*	$('.modal-title').text('Email verification link expired!');
-					$('.modal-body p').html('Sorry! this verification link has expired. <a href="/">Click here</a> to resend the new link for Email verification.');
-					$('#verifyModal').modal('show'); */
-					Blaze.renderWithData(Template.bsmodal, {title: "Email verification link expired!", verifyEmailExpired: true, userId: Meteor.userId()}, $('.modal-dialog')[0]);
+					var user = Meteor.users.find({"services.email.verificationTokens": token});
+					var userId = false;
+					if(user)
+						userId = user._id;
+
+					$('.modal-dialog').empty();
+					Blaze.renderWithData(Template.bsmodal, {title: "Email verification link expired!", verifyEmailExpired: true, userId: user._id}, $('.modal-dialog')[0]);
 					$('#verifyModal').modal('show');
 					console.log('Sorry this verification link has expired. Resend the new link for confirmation?');
 				}
