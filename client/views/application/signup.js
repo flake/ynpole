a8498c41d5ef676a156f371df11d52bbe3ffef4e
Rajasekhar <raj.cynosure@gmail.com>
2015-06-18 09:10:14 +0530

    minor fix fb signin

--- client/views/application/signup.js
+++ client/views/application/signup.js
@@ -1,6 +1,6 @@
 Template.signup.events({
 	'click #facebook-login': function(event, template){
-		Meteor.loginWithFacebook({ requestPermissions: ['email', 'public_profile', 'user_friends', 'user_location', 'user_status', 'read_friendlists']},
+		Meteor.loginWithFacebook({ requestPermissions: ['email', 'public_profile', 'user_friends', 'user_location', 'user_status']},
 			function(err){
 				if(err){
 					return console.log(err);
