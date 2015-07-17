77a96488d6a588af458a030bdcbe4a2343835b1c
Rajasekhar <rajasekahr@ynpole.com>
2015-07-17 13:27:18 +0530

    browser policy updated for images to wildcard *

--- server/init.js
+++ server/init.js
@@ -3,8 +3,9 @@ Meteor.startup(function(){
 	Future = Npm.require('fibers/future');
 
 	BrowserPolicy.content.allowOriginForAll("http://meteor.local");
-	BrowserPolicy.content.allowImageOrigin("https://graph.facebook.com");
-	BrowserPolicy.content.allowImageOrigin("https://lh4.googleusercontent.com");
+	BrowserPolicy.content.allowImageOrigin("*");
+	//BrowserPolicy.content.allowImageOrigin("https://graph.facebook.com");
+	//BrowserPolicy.content.allowImageOrigin("https://lh4.googleusercontent.com");
 
 	// Accounts email config
 	Accounts.emailTemplates.from = 'Ynpole <rajasekhar@ynpole.com>';
